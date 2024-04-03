66aafb73e6f354f41ed9dac5982aa463484ab7ae
uuphoria <155935644+uuphoria2@users.noreply.github.com>
2024-04-03 12:29:58 -0500

    Update retro.js

--- retro.js
+++ retro.js
@@ -1,19 +1,25 @@
-// Check if user prefers dark theme
 const prefersDarkTheme = window.matchMedia("(prefers-color-scheme: dark)");
 
-// Function to toggle dark theme
+function updateDarkModeButtonText() {
+    const darkButton = document.getElementById("dark");
+    if (document.body.classList.contains("dark")) {
+        darkButton.textContent = "Light Mode";
+    } else {
+        darkButton.textContent = "Dark Mode";
+    }
+}
+
 function toggleDarkTheme() {
     document.body.classList.toggle("dark");
     localStorage.setItem('prefersDarkTheme', document.body.classList.contains("dark"));
+    updateDarkModeButtonText();
 }
 
-// Function to toggle retro mode
 function toggleRetroMode() {
     document.body.classList.toggle("retro");
     localStorage.setItem('prefersRetro', document.body.classList.contains("retro"));
 }
 
-// Apply saved preferences on page load
 window.addEventListener("load", () => {
     if (localStorage.getItem('prefersDarkTheme') === 'true' || (localStorage.getItem('prefersDarkTheme') === null && prefersDarkTheme.matches)) {
         document.body.classList.add("dark");
@@ -21,8 +27,8 @@ window.addEventListener("load", () => {
     if (localStorage.getItem('prefersRetro') === 'true') {
         document.body.classList.add('retro');
     }
+    updateDarkModeButtonText();
 });
 
-// Event listeners for theme and retro mode buttons
 document.getElementById("retro").addEventListener("click", toggleRetroMode);
 document.getElementById("dark").addEventListener("click", toggleDarkTheme);
