d03e26149241576369f73446de8e1265fbe726a6
uuphoria <155935644+uuphoria2@users.noreply.github.com>
2024-04-03 16:50:36 -0500

    changes

--- retro.js
+++ retro.js
@@ -1,20 +1,37 @@
-// Check if user prefers dark theme
 const prefersDarkTheme = window.matchMedia("(prefers-color-scheme: dark)");
 
-// Function to toggle dark theme
-function toggleDarkTheme() {
+if (localStorage.getItem('prefersDarkTheme') === 'true' || (localStorage.getItem('prefersDarkTheme') === null && prefersDarkTheme.matches)) {
+    document.body.classList.add("dark");
+    localStorage.setItem('prefersDarkTheme', 'true');
+}
+
+if (localStorage.getItem('prefersRetro') === null) {
+    localStorage.setItem('prefersRetro', 'true');
+}
+
+if (localStorage.getItem('prefersRetro') === 'true') {
+    document.body.classList.add('retro');
+} else {
+    document.body.classList.remove('retro');
+}
+
+if (localStorage.getItem('prefersDarkTheme') === 'true') {
+    document.body.classList.add("dark");
+} else {
+    document.body.classList.remove("dark");
+}
+
+document.getElementById("retro").addEventListener("click", () => {
+    document.body.classList.toggle("retro");
+    localStorage.setItem('prefersRetro', document.body.classList.contains("retro") ? "true" : "false");
+});
+
+document.getElementById("dark").addEventListener("click", () => {
     document.body.classList.toggle("dark");
     localStorage.setItem('prefersDarkTheme', document.body.classList.contains("dark") ? "true" : "false");
     updateModeButtonText();
-}
+});
 
-// Function to toggle retro mode
-function toggleRetroMode() {
-    document.body.classList.toggle("retro");
-    localStorage.setItem('prefersRetro', document.body.classList.contains("retro") ? "true" : "false");
-}
-
-// Function to update mode button text
 function updateModeButtonText() {
     const darkModeButton = document.getElementById('dark');
     if (document.body.classList.contains("dark")) {
@@ -24,16 +41,5 @@ function updateModeButtonText() {
     }
 }
 
-// Apply saved preferences on page load
-window.addEventListener("load", () => {
-    if (localStorage.getItem('prefersDarkTheme') === 'true' || (localStorage.getItem('prefersDarkTheme') === null && prefersDarkTheme.matches)) {
-        document.body.classList.add("dark");
-    }
-    if (localStorage.getItem('prefersRetro') === 'true') {
-        document.body.classList.add('retro');
-    }
-    updateModeButtonText(); // Update mode button text on page load
-});
-
-// Event listener for the Dark Mode button
-document.getElementById("dark").addEventListener("click", toggleDarkTheme);
+// Update mode button text on page load
+updateModeButtonText();
