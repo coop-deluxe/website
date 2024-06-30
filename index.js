8a6aebfeffac4b2ef3f3c08b26a0feadf8917684
Paisley R <90116898+Gapva@users.noreply.github.com>
2024-06-30 11:09:05 -0400

    refactor code, clean-up stylesheet (#8)
    * refactor code, clean-up stylesheet
    
    * fix dx animation; oversight in refactor
    
    * add self as contributor :3
    
    * fix kermeet's profile URL
    
    he updated his username very recently

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
-if (localStorage.getItem('prefersDarkTheme') === 'true') {
-    document.body.classList.add("dark");
-} else {
-    document.body.classList.remove("dark");
+document.body.classList.add('dark'); 
+
+const prefersDark = localStorage.getItem('prefersDarkTheme');
+
+if (prefersDark === 'false') {
+  document.body.classList.remove('dark');
 }
 
-document.getElementById("dark").addEventListener("click", () => {
-    document.body.classList.toggle("dark");
-    if (localStorage.getItem('prefersDarkTheme') === 'true') {
-        localStorage.setItem('prefersDarkTheme', 'false');
-    } else {
-        localStorage.setItem('prefersDarkTheme', 'true');
-    }
+document.getElementById('dark').addEventListener('click', () => {
+    document.body.classList.toggle('dark');
+    const isDark = document.body.classList.contains('dark');
+    localStorage.setItem('prefersDarkTheme', isDark);
 });
